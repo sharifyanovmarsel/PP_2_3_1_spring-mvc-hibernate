861430d2280ae122491c4a849167c3f9202fc140
Sharifyanov Marsel <sharifyanovmarsel@gmail.com>
2025-01-13 16:44:32 +0500

    Initial commit

---
+++ src/main/java/com/sharifyanov/springcourse/controllers/FirstController.java
@@ -0,0 +1,20 @@
+package com.sharifyanov.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/firstController")
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
---
+++ src/main/java/com/sharifyanov/springcourse/models/Person.java
@@ -0,0 +1,65 @@
+package com.sharifyanov.springcourse.models;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotEmpty;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2, max = 30, message = "name should be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age shold be greater than zero")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
